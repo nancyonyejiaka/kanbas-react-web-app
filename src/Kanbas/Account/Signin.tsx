4f1c80d47eacaa53e4404ea00f35f4e836d7448e
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2024-07-28 19:25:39 -0400

    bug hunting

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -20,10 +20,14 @@ export default function Signin() {
 
     try {
       const currentUser = await client.signin(credentials);
-      dispatch(setCurrentUser(currentUser));
-      navigate('/Kanbas/Account/Profile');
-    } catch (err: any) {
-      setErrors(err.response.data.message);
+      if (currentUser) {
+        dispatch(setCurrentUser(currentUser));
+        navigate('/Kanbas/Account/Profile');
+      } else {
+        setErrors({ general: 'Invalid username or password' });
+      }
+    } catch (error) {
+      setErrors({ general: 'Signin failed. Please try again.' });
     }
   };
 
